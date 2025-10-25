97eb081d705c5a66093f110853670c22010d664c
zebra <victoras.isaakidis@sysdocgroup.com>
2025-10-25 20:13:13 +0300

    create a tcp layer class

--- websocket-server/TcpLayer.cs
+++ websocket-server/TcpLayer.cs
@@ -1,6 +1,22 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace server;
 
-public class TcpLayer
+public class TcpLayer(IPAddress address, int port)
 {
-    // here we will handle everything that has to do with the tcp layer: Listeting for tcp connections, accept client, getting the stream.
+    private readonly byte[] _bytes = new byte[1024];
+    private readonly TcpListener _tcpListener = new TcpListener(address, port);
+    
+    public TcpClient CreateTcpConnection()
+    {
+        _tcpListener.Start();
+
+        while (true)
+        {
+            Console.WriteLine("Listening on port {0}", address + ":" + port);
+            var tcpClient = _tcpListener.AcceptTcpClient();
+            return tcpClient;
+        }
+    }
 }
