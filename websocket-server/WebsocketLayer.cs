278611d1365ca6482e1b4b56267b209a3b984261
zebra <victoras.isaakidis@sysdocgroup.com>
2025-10-26 23:37:42 +0200

    initial implementation of fragmentation

--- websocket-server/WebsocketLayer.cs
+++ websocket-server/WebsocketLayer.cs
@@ -1,8 +1,10 @@
+using System.Net.Sockets;
+using System.Net.WebSockets;
 using System.Text;
 
 namespace server;
 
-public class WebsocketLayer
+public class WebsocketLayer()
 {
     public string HandleWebsocketLayer(ConnectionHandler connectionHandler)
     {
@@ -10,31 +12,28 @@ public class WebsocketLayer
         byte[] frameBuffer = new byte[1024];
         int bytesRead = connectionHandler.NetworkStream.Read(frameBuffer, 0, frameBuffer.Length);
         messageBuffer.AddRange(frameBuffer.Take(bytesRead));
+        
+        // we need to find the FIN. The FIN it shows if this is the last frame or there are also more frames coming. If it's 0 means more messages will follow.
+        // if it's 1 means this is the last one. The 2nd, 3rd and 4th bit is reserved, rarely used. And the last 4 bits are use for the opcodes.
+        bool isFinalFrame = (frameBuffer[0] & 0b10000000) != 0;
+        int opcode = (frameBuffer[0] & 0b00001111);
+
+        if (!isFinalFrame && opcode != 0)
+        {
+            messageBuffer.AddRange(frameBuffer);
+        } else if (!isFinalFrame && opcode == 0)
+        {
+            messageBuffer.AddRange(frameBuffer);   
+        } else if (isFinalFrame && opcode != 0)
+        {
+            messageBuffer.AddRange(frameBuffer);
+        }
+        
         return DecodeFrame(messageBuffer);
     }
     
     private static string DecodeFrame(List<byte> frameBuffer)
     {
-        foreach (var t in frameBuffer)
-        {
-            Console.WriteLine(t);
-        }
-        // we need to find the FIN. The FIN it shows if this is the last frame or there are also more frames coming. If it's 0 means more messages will follow.
-        // if it's 1 means this is the last one. The 2nd, 3rd and 4th bit is reserved, rarely used. And the last 4 bits are use for the opcodes.
-        bool isFinalFrame = (frameBuffer[0] & 0b10000000) != 0;
-        Console.WriteLine($"Is this the final frame? {isFinalFrame}");
-        int opcode = (frameBuffer[0] & 0b00001111);
-        if (opcode == 1)
-        {
-            string dataType = "text";
-            Console.WriteLine($"The payload data type is {dataType} {opcode}");
-        }
-        else
-        {
-            Console.WriteLine($"Check for other opcode types {opcode}");
-            // maybe store in buffer?
-        };
-        
         // the 2nd byte is for the MASK and the payload length. The MASK just takes the first bit.
         bool isPayloadDataMasked = (frameBuffer[1] & 0b10000000) != 0;
         Console.WriteLine($"Is payload masked? {isPayloadDataMasked}");
