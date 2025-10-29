a450d2ac89064e96b1221cab0aceb158d72ad451
zebra <victoras.isaakidis@sysdocgroup.com>
2025-10-29 22:32:16 +0200

    add new files into program cs

--- websocket-server/Program.cs
+++ websocket-server/Program.cs
@@ -11,35 +11,30 @@ public abstract class Start
     {
         try
         {
-            var tcpLayer = new TcpLayer(IPAddress.Any);
-            var client = await tcpLayer.CreateTcpConnection();
-            var connectionHandler = new ConnectionHandler(client);
-            var websocketLayer = new WebsocketLayer();
-            var request = await connectionHandler.HandleTcpClient();
-            var httpHandler = new HttpHandler(connectionHandler);
-            var requestHeader = await httpHandler.HandleRequestHeader(request);
-
-            if (requestHeader.Headers.ContainsKey("upgrade") && 
-                requestHeader.Headers.ContainsKey("connection")&&
-                requestHeader.Headers["upgrade"].ToLower() == "websocket" && 
-                requestHeader.Headers["connection"].ToLower() == "upgrade")
-            {
-                httpHandler.CreateHttpResponseHeader(requestHeader);
-                
-                while (client.Connected)
-                {
-                    await websocketLayer.HandleWebsocketLayer(connectionHandler);
-                }
-            }
-            else
-            {
-                var response = requestHeader.HttpVersion + " " + "200" + " " + "OK" + httpHandler.NextLine + "Content-Type: " + "text/plain" + httpHandler.NextLine + "Hello from C# server!";
-                connectionHandler.NetworkStream.Write(Encoding.UTF8.GetBytes(response), 0, response.Length);
-                connectionHandler.NetworkStream.Close();
-                client.Close();
-            }
             
+                var tcpLayer = new TcpLayer(IPAddress.Any);
+                var client = await tcpLayer.CreateTcpConnection();
+                var connectionHandler = new ConnectionHandler(client);
+                var httpHandler = new HttpHandler(connectionHandler);
+                var requestHeader = await httpHandler.ReadHttpRequest();
+
+                if (requestHeader.Headers.ContainsKey("upgrade") && 
+                    requestHeader.Headers.ContainsKey("connection")&&
+                    requestHeader.Headers["upgrade"].ToLower() == "websocket" && 
+                    requestHeader.Headers["connection"].ToLower() == "upgrade")
+                {
+                    httpHandler.CreateHttpResponseHeader(requestHeader);
+                    await connectionHandler.RunWebsocketLoop();
+                }
+                else
+                {
+                    var response = requestHeader.HttpVersion + " " + "200" + " " + "OK" + httpHandler.NextLine + "Content-Type: " + "text/plain" + httpHandler.NextLine + "Hello from C# server!";
+                    connectionHandler.NetworkStream.Write(Encoding.UTF8.GetBytes(response), 0, response.Length);
+                    connectionHandler.NetworkStream.Close();
+                    client.Close();
+                }
         }
+
         catch (Exception e)
         {
             Console.WriteLine(e);
