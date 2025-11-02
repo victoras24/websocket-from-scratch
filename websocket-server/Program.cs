979771cb13a53d93e0679d8377af90243782c769
zebra <victoras.isaakidis@sysdocgroup.com>
2025-11-02 12:42:56 +0200

    add the connection handler in the tcp layer loop so each time a new client connects a new tcp connection is created

--- websocket-server/Program.cs
+++ websocket-server/Program.cs
@@ -11,28 +11,8 @@ public abstract class Start
     {
         try
         {
-            
                 var tcpLayer = new TcpLayer(IPAddress.Any);
-                var client = await tcpLayer.CreateTcpConnection();
-                var connectionHandler = new ConnectionHandler(client);
-                var httpHandler = new HttpHandler(connectionHandler);
-                var requestHeader = await httpHandler.ReadHttpRequest();
-
-                if (requestHeader.Headers.ContainsKey("upgrade") && 
-                    requestHeader.Headers.ContainsKey("connection")&&
-                    requestHeader.Headers["upgrade"].ToLower() == "websocket" && 
-                    requestHeader.Headers["connection"].ToLower() == "upgrade")
-                {
-                    httpHandler.CreateHttpResponseHeader(requestHeader);
-                    await connectionHandler.RunWebsocketLoop();
-                }
-                else
-                {
-                    var response = requestHeader.HttpVersion + " " + "200" + " " + "OK" + httpHandler.NextLine + "Content-Type: " + "text/plain" + httpHandler.NextLine + "Hello from C# server!";
-                    connectionHandler.NetworkStream.Write(Encoding.UTF8.GetBytes(response), 0, response.Length);
-                    connectionHandler.NetworkStream.Close();
-                    client.Close();
-                }
+                await tcpLayer.CreateTcpConnection();
         }
 
         catch (Exception e)
--- websocket-server/TcpLayer.cs
+++ websocket-server/TcpLayer.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace server;
 
@@ -12,12 +13,30 @@ public class TcpLayer(IPAddress address)
     public async Task<TcpClient> CreateTcpConnection()
     {
         _tcpListener.Start();
-
+        
         while (true)
         {
             Console.WriteLine("Listening on port {0}", address + ":" + Port);
             var tcpClient = await _tcpListener.AcceptTcpClientAsync();
-            return tcpClient;
+            var connectionHandler = new ConnectionHandler(tcpClient);
+            var httpHandler = new HttpHandler(connectionHandler);
+            var requestHeader = await httpHandler.ReadHttpRequest();
+
+            if (requestHeader.Headers.ContainsKey("upgrade") && 
+                requestHeader.Headers.ContainsKey("connection")&&
+                requestHeader.Headers["upgrade"].ToLower() == "websocket" && 
+                requestHeader.Headers["connection"].ToLower() == "upgrade")
+            {
+                httpHandler.CreateHttpResponseHeader(requestHeader);
+                connectionHandler.RunWebsocketLoop();
+            }
+            else
+            {
+                var response = requestHeader.HttpVersion + " " + "200" + " " + "OK" + httpHandler.NextLine + "Content-Type: " + "text/plain" + httpHandler.NextLine + "Hello from C# server!";
+                connectionHandler.NetworkStream.Write(Encoding.UTF8.GetBytes(response), 0, response.Length);
+                connectionHandler.NetworkStream.Close();
+                tcpClient.Close();
+            }
         }
     }
 }
